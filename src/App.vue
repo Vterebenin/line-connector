3ec6d30372343179c1f36f2f96450317b36a9944
Vterebenin <niceserver11@mail.ru>
2019-08-21 21:59:44 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -16,13 +9,14 @@ export default {
 }
 </script>
 
+<template>
+  <div id="app">
+    <HelloWorld msg="sadfsadf" someotherthing="true" />
+  </div>
+</template>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

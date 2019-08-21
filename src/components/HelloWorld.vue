d99c86296904b3d0eb9db94a1d800a2578503136
Vterebenin <niceserver11@mail.ru>
2019-08-22 01:04:44 +0300

    now they are connected

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,14 +2,27 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      {{ someotherthing }}
-      <svg id="test" style="position: absolute; left: 0; top: 0;" height="100%" width="100%" >
+      {{ from.x }}
+      <svg id="test" class="drawer" height="100%" width="100%">
+        <path v-bind:d="'M ' + from.x + ' ' + from.y + ' C 120 140, 180 140, ' + to.x + ' ' + to.y" stroke="black" fill="transparent" />
         <circle
+          class="drawer__circle drawer__circle-1"
           cx="50"
           cy="50"
           r="40"
           v-on:dragstart="dragstartFalse"
-          v-on:mousedown="mousedownn($event)"
+          v-on:mousedown="mouseDownOnCircle($event)"
+          stroke="black"
+          stroke-width="3"
+          fill="red"
+        />
+        <circle
+          class="drawer__circle drawer__circle-2"
+          cx="150"
+          cy="150"
+          r="80"
+          v-on:dragstart="dragstartFalse"
+          v-on:mousedown="mouseDownOnCircle($event)"
           stroke="black"
           stroke-width="3"
           fill="red"
@@ -22,36 +35,84 @@
 <script>
 export default {
   name: "HelloWorld",
+  data: function() {
+    return {
+      time: 0,
+      from: {
+        x: 0,
+        y: 0
+      },
+      to: {
+        x: 0,
+        y: 0
+      }
+    }
+  },
+
   props: {
     msg: String,
     someotherthing: String
   },
-  methods: {
-    debugMessage() {
+  computed: {
+    pathFrom: {
+      get: function(newVal) {
+        return (this.from = { x: 0, y: 0 });
+      },
+      set: function(newVal) {
+        return (this.from = newVal);
+      }
     },
-    mousedownn($event) {
+    pathTo: {
+      get: function(newVal) {
+        return (this.to = { x: 0, y: 0 });
+      },
+      set: function(newVal) {
+        return (this.to = newVal);
+      }
+    }
+    // from: function(params) {}
+  },
+  created: function() {
+    // axios.get('symbols.json').then(response => this.symbols = response.data);
+      this.time = Date.now
+    console.log("test")
+  },
+  methods: {
+    debugMessage() {},
+    mouseDownOnCircle($event) {
       let ball = event.target;
-      let radius = ball.getAttribute("r")
-      console.log(radius)
-      let svg = document.getElementById("test")
-      console.log(ball.getBoundingClientRect().left)
+      let ball1 = document.getElementsByClassName("drawer__circle-1")[0]
+      
+      let ball2 = document.getElementsByClassName("drawer__circle-2")[0]
+      let radius = ball.getAttribute("r");
+      let svg = document.getElementById("test");
+      const reducedThis = this;
 
       let shiftX = event.clientX - ball.getBoundingClientRect().left - radius;
-      let shiftY = event.clientY - ball.getBoundingClientRect().top  - radius
+      let shiftY = event.clientY - ball.getBoundingClientRect().top - radius;
 
       svg.append(ball);
 
-      moveAt(event.pageX, event.pageY);
+      moveAt(event.pageX, event.pageY, reducedThis);
 
       // moves the ball at (pageX, pageY) coordinates
       // taking initial shifts into account
-      function moveAt(pageX, pageY) {
+      function moveAt(pageX, pageY, reducedThis) {
         ball.setAttribute("cx", pageX - shiftX);
         ball.setAttribute("cy", pageY - shiftY);
+        console.log(reducedThis)
+        reducedThis.pathFrom = {
+          x: ball1.getAttribute("cx"),
+          y: ball1.getAttribute("cy")
+        }
+        reducedThis.pathTo = {
+          x: ball2.getAttribute("cx"),
+          y: ball2.getAttribute("cy")
+        }
       }
 
       function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
+        moveAt(event.pageX, event.pageY, reducedThis);
       }
 
       // move the ball on mousemove
@@ -71,5 +132,9 @@ export default {
 </script>
 
 <style scoped>
-
+.drawer {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
 </style>

217e710ee8ff5d6399e431b5f19f4db4e0c650cb
VaTerebenin <te@web-canape.com>
2019-08-22 17:53:47 +0300

    add bezier

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,11 +4,50 @@
     <p>
       {{ from.x }}
       <svg id="test" class="drawer" height="100%" width="100%">
-        <path v-bind:d="'M ' + from.x + ' ' + from.y + ' C 120 140, 180 140, ' + to.x + ' ' + to.y" stroke="black" fill="transparent" />
+        <path
+          v-bind:d="'M ' + from.x + ' ' + from.y + ' C ' + bezierFrom.x + ' ' + bezierFrom.y + ', ' + bezierTo.x + ' ' + bezierTo.y + ', ' + to.x + ' ' + to.y"
+          stroke="black"
+          fill="transparent"
+        />
+        <!-- безье хелпер 1 -->
+        <path
+          v-bind:d="'M ' + from.x + ' ' + from.y + ' ' + bezierFrom.x + ' ' + bezierFrom.y"
+          stroke="black"
+          fill="transparent"
+        />
+        <!-- безье хелпер 2 -->
+        <path
+          v-bind:d="'M ' + to.x + ' ' + to.y + ' ' + bezierTo.x + ' ' + bezierTo.y"
+          stroke="black"
+          fill="transparent"
+        />
+        <!-- точка для хелпера 1 -->
+        <circle
+          v-bind:cx="bezierFrom.x"
+          v-bind:cy="bezierFrom.y"
+          v-on:dragstart="dragstartFalse"
+          v-on:mousedown="mouseDownOnBezier('bezierFrom')"
+          r="5"
+          stroke="black"
+          stroke-width="3"
+          fill="red"
+        />
+        <!-- точка для хелпера2 -->
+        <circle
+          v-bind:cx="bezierTo.x"
+          v-bind:cy="bezierTo.y"
+          v-on:dragstart="dragstartFalse"
+          v-on:mousedown="mouseDownOnBezier('bezierTo')"
+          r="5"
+          stroke="black"
+          stroke-width="3"
+          fill="red"
+        />
+        <!-- маленький кружок -->
         <circle
           class="drawer__circle drawer__circle-1"
-          cx="50"
-          cy="50"
+          v-bind:cx="from.x"
+          v-bind:cy="from.y"
           r="40"
           v-on:dragstart="dragstartFalse"
           v-on:mousedown="mouseDownOnCircle($event)"
@@ -16,10 +55,11 @@
           stroke-width="3"
           fill="red"
         />
+        <!-- большой кружок -->
         <circle
           class="drawer__circle drawer__circle-2"
-          cx="150"
-          cy="150"
+          v-bind:cx="to.x"
+          v-bind:cy="to.y"
           r="80"
           v-on:dragstart="dragstartFalse"
           v-on:mousedown="mouseDownOnCircle($event)"
@@ -37,16 +77,23 @@ export default {
   name: "HelloWorld",
   data: function() {
     return {
-      time: 0,
       from: {
-        x: 0,
-        y: 0
+        x: 150,
+        y: 150
       },
       to: {
-        x: 0,
-        y: 0
+        x: 250,
+        y: 250
+      },
+      bezierFrom: {
+        x: 120,
+        y: 140
+      },
+      bezierTo: {
+        x: 180,
+        y: 140
       }
-    }
+    };
   },
 
   props: {
@@ -55,35 +102,50 @@ export default {
   },
   computed: {
     pathFrom: {
-      get: function(newVal) {
-        return (this.from = { x: 0, y: 0 });
+      get: function() {
+        return this.from;
       },
       set: function(newVal) {
         return (this.from = newVal);
       }
     },
     pathTo: {
-      get: function(newVal) {
-        return (this.to = { x: 0, y: 0 });
+      get: function() {
+        return this.to;
       },
       set: function(newVal) {
         return (this.to = newVal);
+      }
+    },
+    bezierFromC: {
+      get: function() {
+        return this.bezierFrom;
+      },
+      set: function(newVal) {
+        return (this.bezierFrom = newVal);
+      }
+    },
+    bezierToC: {
+      get: function() {
+        return this.bezierTo;
+      },
+      set: function(newVal) {
+        return (this.bezierTo = newVal);
       }
     }
     // from: function(params) {}
   },
   created: function() {
     // axios.get('symbols.json').then(response => this.symbols = response.data);
-      this.time = Date.now
-    console.log("test")
+    this.time = Date.now;
   },
   methods: {
     debugMessage() {},
-    mouseDownOnCircle($event) {
+    mouseDownOnCircle() {
       let ball = event.target;
-      let ball1 = document.getElementsByClassName("drawer__circle-1")[0]
-      
-      let ball2 = document.getElementsByClassName("drawer__circle-2")[0]
+      let ball1 = document.getElementsByClassName("drawer__circle-1")[0];
+
+      let ball2 = document.getElementsByClassName("drawer__circle-2")[0];
       let radius = ball.getAttribute("r");
       let svg = document.getElementById("test");
       const reducedThis = this;
@@ -100,19 +162,62 @@ export default {
       function moveAt(pageX, pageY, reducedThis) {
         ball.setAttribute("cx", pageX - shiftX);
         ball.setAttribute("cy", pageY - shiftY);
-        console.log(reducedThis)
         reducedThis.pathFrom = {
           x: ball1.getAttribute("cx"),
           y: ball1.getAttribute("cy")
-        }
+        };
         reducedThis.pathTo = {
           x: ball2.getAttribute("cx"),
           y: ball2.getAttribute("cy")
-        }
+        };
       }
 
       function onMouseMove(event) {
         moveAt(event.pageX, event.pageY, reducedThis);
+      }
+
+      // move the ball on mousemove
+      document.addEventListener("mousemove", onMouseMove);
+
+      // drop the ball, remove unneeded handlers
+      ball.onmouseup = function() {
+        document.removeEventListener("mousemove", onMouseMove);
+        ball.onmouseup = null;
+      };
+    },
+    mouseDownOnBezier(coords) {
+      let ball = event.target;
+      let radius = ball.getAttribute("r");
+      let svg = document.getElementById("test");
+
+      console.log(coords);
+
+      let shiftX = event.clientX - ball.getBoundingClientRect().left - radius;
+      let shiftY = event.clientY - ball.getBoundingClientRect().top - radius;
+      const reducedThis = this;
+
+      svg.append(ball);
+
+      moveAt(event.pageX, event.pageY, reducedThis, coords);
+
+      console.log(this, "asd");
+
+      // moves the ball at (pageX, pageY) coordinates
+      // taking initial shifts into account
+      function moveAt(pageX, pageY, reducedThis, coordi) {
+        ball.setAttribute("cx", pageX - shiftX);
+        ball.setAttribute("cy", pageY - shiftY);
+        console.log(reducedThis[coordi]);
+        
+        reducedThis[coordi] = {
+          x: ball.getAttribute("cx"),
+          y: ball.getAttribute("cy")
+        };
+        console.log(reducedThis);
+      }
+
+      function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY, reducedThis, coords);
       }
 
       // move the ball on mousemove

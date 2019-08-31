29f6742cdbe1e399c07d29ae633bae1caea394ad
Vterebenin <niceserver11@mail.ru>
2019-08-31 15:55:37 +0300

    refractoring

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import EditablePathLine from "./components/EditablePathLine.vue";
 export default {
   name: "app",
   components: {
-    EditablePathLine,
+    EditablePathLine
   }
 };
 </script>
@@ -11,9 +11,24 @@ export default {
 <template>
   <div id="app">
     <svg id="test" class="drawer" height="100%" width="100%">
-      <EditablePathLine :text="`test1`" :pathId="1" :radius="20" :position="{x1: 50, y1: 150, x2: 200, y2: 350 }" msg="sadfsadf" someotherthing="true" />
-      <EditablePathLine :text="`test2`" :pathId="2" :radius="40" :position="{x1: 600, y1: 150, x2: 800, y2: 350 }" msg="sadfsadf" someotherthing="true" />
+      <EditablePathLine
+        :text="`test1`"
+        :pathId="1"
+        :radius="20"
+        :position="{x1: 50, y1: 150, x2: 200, y2: 350 }"
+        msg="sadfsadf"
+        someotherthing="true"
+      />
+      <EditablePathLine
+        :text="`test2`"
+        :pathId="2"
+        :radius="40"
+        :position="{x1: 600, y1: 150, x2: 800, y2: 350 }"
+        msg="sadfsadf"
+        someotherthing="true"
+      />
     </svg>
+    
   </div>
 </template>
 
